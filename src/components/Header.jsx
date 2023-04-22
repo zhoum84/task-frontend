38ed4f5d1bed26143eb85b4fe6239faef78f88eb
satkalra <satyam_kalra@yahoo.co.in>
2023-04-22 19:11:20 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'>Task Tracker </Link>
+        <Link to={isUserLoggedIn? '/home' : '/'}>Task Tracker </Link>
       </div>
       <div>
         {user}
